4487e29db624513fa368ccb8210610727c3a9766
Eduardo Santana <edusf0@hotmail.com>
2022-05-26 15:48:42 -0300

    Updates

---
+++ precache-manifest.eca58b749aee4c10e1c4f277cfc2a97e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b7f37a2f848d770c8b8",
+    "url": "/buscador-de-cep/static/css/main.77b25e43.chunk.css"
+  },
+  {
+    "revision": "5b7f37a2f848d770c8b8",
+    "url": "/buscador-de-cep/static/js/main.5b7f37a2.chunk.js"
+  },
+  {
+    "revision": "66a24c1cf6e5afaa141e",
+    "url": "/buscador-de-cep/static/js/1.66a24c1c.chunk.js"
+  },
+  {
+    "revision": "b76857559a860db1e9e5",
+    "url": "/buscador-de-cep/static/js/runtime~main.b7685755.js"
+  },
+  {
+    "revision": "ed7fadb390a2a082b01551a6f1482c66",
+    "url": "/buscador-de-cep/index.html"
+  }
+];
